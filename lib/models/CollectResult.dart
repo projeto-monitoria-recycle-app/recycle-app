bf78dd3e0a0951abd9ea173a785fe9ae41923a13
Thales Azevedo <thales.azevedo@hotmail.com>
2021-05-31 17:21:56 -0300

    Formatting code

--- lib/models/CollectResult.dart
+++ lib/models/CollectResult.dart
@@ -1,22 +1,30 @@
 import 'dart:core';
 
-class CollectResult{
+class CollectResult {
   final double quantidade;
   final String mes;
 
   CollectResult(
-      this.quantidade,
-      this.mes,
-      );
+    this.quantidade,
+    this.mes,
+  );
 }
+
 List<String> meses = [
-  "Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov","Dez",
+  "Jan",
+  "Fev",
+  "Mar",
+  "Abr",
+  "Mai",
+  "Jun",
+  "Jul",
+  "Ago",
+  "Set",
+  "Out",
+  "Nov",
+  "Dez",
 ];
 
-List<int> indiceMateriais =[
-  0,1,2,3,5,8
-];
+List<int> indiceMateriais = [0, 1, 2, 3, 4, 7];
 
-List<String> periodos =[
-  "2020-1", "2020-2"
-];
+List<String> periodos = ["2020-1", "2020-2"];
